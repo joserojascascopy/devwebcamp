cc62693eed1758693fa3fefae336a7c703207497
joserojascascopy <joserojascascopy@hotmail.com>
2025-07-30 19:22:23 -0300

    fix: corregir configuración de gulp y versiones de paquetes para compatibilidad CommonJS

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,45 @@
+// CSS
 const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+// IMAGENES
+const imagemin = require('gulp-imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminOptipng = require('imagemin-optipng');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css() {
     return src('./app/assets/scss/**/*.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({ style: 'compressed' }).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('./public/build/css'));
+}
+
+function imagenes() {
+    return src('./app/assets/img/**/*.{png,jpg}')
+        .pipe(imagemin([
+            imageminMozjpeg({quality: 75}),
+            imageminOptipng({optimizationLevel: 3})
+        ]))
+        .pipe(dest('./public/build/img'))
+}
+
+function versionWebp() {
+    return src('./app/assets/img/**/*.{png,jpg}')
+        .pipe(webp({quality: 50}))
+        .pipe(dest('./public/build/img'))
+}
+
+function versionAvif() {
+    return src('./app/assets/img/**/*.{png,jpg}')
+        .pipe(avif({quality: 50}))
+        .pipe(dest('./public/build/img'))
 }
 
 function js() {
@@ -18,6 +50,9 @@ function js() {
 function watchArchivos() {
     watch('./app/assets/scss/**/*.scss', css);
     watch('./app/assets/js/**/*.js', js);
+    watch('./app/assets/img/**/*', imagenes);
+    watch('./app/assets/img/**/*', versionWebp);
+    watch('./app/assets/img/**/*', versionAvif);
 }
 
-exports.default = parallel(css, js, watchArchivos);
+exports.default = parallel(css, js, imagenes, versionWebp, versionAvif, watchArchivos);
